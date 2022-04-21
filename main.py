f94346e8fa4130500f4f08a1b764915a41f4a6bf
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2022-04-21 00:49:14 -0600

    started doing some weird code shit

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import tkinter as tk
-import random
-import time
-from threading import Thread
 import pathlib
-import pygame
-from pygame.locals import *
 
-pygame.init()
+def main():
 
-class Player(pygame.sprite.Group):
+    # Window settings
+    window = tk.Tk()
+    window.title("Taller de Interfaz Gráfica")
+    window.geometry("448x512")
+    #window.iconbitmap( str(pathlib.Path().resolve()) + "\icon.ico")
+    window.resizable(False, False)
 
-    def __init__(self, color, width, height):
-        
+    #Canvases and stuff like that
+    menu_canvas = tk.Canvas(window, width=448, height=512, borderwidth=0, highlightthickness=0, bg="#000000")
+    menu_canvas.place(x=0, y=0)
+
+    # The window we are working with must be selected as the main loop
+    window.mainloop()
+
+main()
