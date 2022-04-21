74514e7fce3889e9296440de8f8635731289df5d
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2022-04-21 12:20:23 -0600

    added level bg and some sprites

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-import tkinter as tk
-import pathlib
+import pygame
+from sys import exit
 
-def main():
+pygame.init()
 
-    # Window settings
-    window = tk.Tk()
-    window.title("Taller de Interfaz Gráfica")
-    window.geometry("448x512")
-    #window.iconbitmap( str(pathlib.Path().resolve()) + "\icon.ico")
-    window.resizable(False, False)
+#Sets the Screen Size and Icon
+screen = pygame.display.set_mode((448, 512))
+pygame.display.set_caption("Monkey")
+#pygame.display.set_icon("icon.ico")
 
-    #Canvases and stuff like that
-    menu_canvas = tk.Canvas(window, width=448, height=512, borderwidth=0, highlightthickness=0, bg="#000000")
-    menu_canvas.place(x=0, y=0)
+#This helps us set the Framerate
+clock = pygame.time.Clock()
 
-    # The window we are working with must be selected as the main loop
-    window.mainloop()
+#Setting the images for the game so far
+bg_surf = pygame.image.load("images/level1.png").convert_alpha()
+bg_rect = bg_surf.get_rect(topleft = (0, 0))
 
-main()
+player_surf = pygame.image.load("images/mario/mario1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 496))
+
+while True:
+
+    #Checks input from the player
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(bg_surf, bg_rect)
+    screen.blit(player_surf, player_rect)
+
+    pygame.display.update()
+    clock.tick(60)
