387c2e2314e8fcba0fc5c7923f1116c183f10b58
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2022-04-22 18:31:55 -0600

    Finished the Pygame Tutorial

--- main.py
+++ main.py
@@ -1,89 +1,207 @@
-from cmath import rect
 import pygame
 from sys import exit
+from random import randint, choice
+
+class Player(pygame.sprite.Sprite): 
+    def __init__(self):
+        super().__init__()
+        #imports the sprites
+        player_walk1 = pygame.image.load("images/mario/skg-run1.png").convert_alpha()
+        player_walk2 = pygame.image.load("images/mario/skg-run2.png").convert_alpha()
+        player_walk3 = pygame.image.load("images/mario/skg-run3.png").convert_alpha()
+        self.player_walk = [player_walk1, player_walk2, player_walk3]
+        self.player_index = 0
+        self.player_jump = pygame.image.load("images/mario/skg-jump.png").convert_alpha()
+
+        #Sets up the player itself
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom = (80, 496))
+        self.gravity = 0
+        self.speed = 0
+
+    def player_input(self):
+        self.keys = pygame.key.get_pressed()
+
+        #Checks input for jumping
+        if self.keys[pygame.K_SPACE] and self.rect.bottom == 496 and self.keys[pygame.K_LEFT]:
+            self.gravity = -4.5
+            self.speed = -1
+        
+        elif self.keys[pygame.K_SPACE] and self.rect.bottom == 496 and self.keys[pygame.K_RIGHT]:
+            self.gravity = -4.5
+            self.speed = 1
+        
+        elif self.keys[pygame.K_SPACE] and self.rect.bottom == 496:
+            self.gravity = -4.5
+
+        #Checks imput for movement
+        if self.keys[pygame.K_RIGHT] or self.keys[pygame.K_d]:
+            self.speed = 3
+        
+        elif self.keys[pygame.K_LEFT] or self.keys[pygame.K_a]:
+            self.speed = -3
+
+        elif self.rect.bottom == 496:
+            self.speed = 0
+
+    def apply_movement(self):
+        self.gravity += 0.3
+        self.rect.bottom += self.gravity
+        self.rect.x += self.speed
+        
+        if self.rect.bottom >= 496:
+            self.rect.bottom = 496
+
+    def animation_state(self):
+        if self.rect.bottom < 496:
+            self.image = self.player_jump
+        
+        elif self.rect.bottom == 496 and (self.keys[pygame.K_a] or self.keys[pygame.K_d] or self.keys[pygame.K_RIGHT] or self.keys[pygame.K_LEFT]):
+            self.player_index += 0.151666666666
+            
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+        
+        else:
+            self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_movement()
+        self.animation_state()
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):     
+        super().__init__()   
+        if type == "blue barrel":
+            blue_barrel_frame1 = pygame.image.load("images/barrel/bluebarrel1.png").convert_alpha()
+            blue_barrel_frame2 = pygame.image.load("images/barrel/bluebarrel2.png").convert_alpha()
+            blue_barrel_frame3 = pygame.image.load("images/barrel/bluebarrel3.png").convert_alpha()
+            blue_barrel_frame4 = pygame.image.load("images/barrel/bluebarrel4.png").convert_alpha()
+            self.frames = [blue_barrel_frame1, blue_barrel_frame2, blue_barrel_frame3, blue_barrel_frame4]
+            ypos = 456
+
+        else:
+            #Obstacles
+            barrel_frame1 = pygame.image.load("images/barrel/barrel1.png").convert_alpha()
+            barrel_frame2 = pygame.image.load("images/barrel/barrel2.png").convert_alpha()
+            barrel_frame3 = pygame.image.load("images/barrel/barrel3.png").convert_alpha()
+            barrel_frame4 = pygame.image.load("images/barrel/barrel4.png").convert_alpha()
+            self.frames = [barrel_frame1, barrel_frame2, barrel_frame3, barrel_frame4]
+            ypos = 496
+
+        self.animation_index = 0    
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(648, 800), ypos))
+
+    def animation_state(self):
+            self.animation_index += 0.151666666666
+            
+            if self.animation_index >= len(self.frames):
+                self.animation_index = 0
+            self.image = self.frames[int(self.animation_index)]
+
+    def destroy(self):
+
+        if self.rect.x <= -50:
+            self.kill()
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 2.5
+        self.destroy()
+
+def collision_sprite():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else:
+        return True
 
 pygame.init()
 
 #Sets the Screen Size and Icon
 screen = pygame.display.set_mode((448, 512))
-pygame.display.set_caption("Depressed Monkey and boi")
+pygame.display.set_caption("Depressed Artist")
 #pygame.display.set_icon("icon.ico")
-game_active = True
+game_active = False
 
 #This helps us set the Framerate
 clock = pygame.time.Clock()
+
+score = 0
+
+#Groups
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+obstacle_group = pygame.sprite.Group()
 
 #Setting the images for the game so far
 bg_surf = pygame.image.load("images/level1.png").convert_alpha()
 bg_rect = bg_surf.get_rect(topleft = (0, 0))
 
-player_surf = pygame.image.load("images/mario/mario1.png").convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (80, 496))
-player_grav = 0
-player_speed = 0
-isGround = False
+score_text = pygame.font.Font("font/W95FA.otf", 30)
+score_surf = score_text.render("Score: " + str(score), False, "#afafaf")
+score_rect = score_surf.get_rect(center = (224, 30))
 
-barrel_surf = pygame.image.load("images/barrel/barrel1.png").convert_alpha()
-barrel_rect = barrel_surf.get_rect(midbottom = (440, 496))
+#Intro Screen
+player_stand = pygame.image.load("images/mario/skg-run1.png").convert_alpha()
+player_stand = pygame.transform.scale2x(player_stand)
+player_stand = pygame.transform.scale2x(player_stand)
+#player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center = (224, 250))
 
-test_font = pygame.font.Font("font/W95FA.otf", 30)
-score_surf = test_font.render("lmao monke", False, "White")
-score_rect = score_surf.get_rect(midtop = (224, 10))
+game_name = pygame.font.Font("font/W95FA.otf", 50)
+name_surf = game_name.render("Depressed Artist :,)", False, "White")
+name_rect = name_surf.get_rect(midtop = (224, 30))
+
+inst_font = pygame.font.Font("font/W95FA.otf", 30)
+inst_surf = inst_font.render("Press Space to start", False, "#afafaf")
+inst_rect = inst_surf.get_rect(center = (224, 350))
+
+#Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
 
 while True:
-
 
     #Checks input from the player
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
 
         if game_active:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    pygame.transform.flip(player_surf, False, False)
-                    player_speed = -3
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    pygame.transform.flip(player_surf, True, False)
-                    player_speed = 3
-
-            else:
-                player_speed = 0
-                
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and isGround:
-                    player_grav = -4.5
-                    isGround = False
+            if event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(["blue barrel", "barrel", "barrel"])))
 
     if game_active:
 
-        barrel_rect.x -= 2.5
-
-        if barrel_rect.right <= 0:
-            barrel_rect.left = 448
-
         screen.blit(bg_surf, bg_rect)
         screen.blit(score_surf, score_rect)
-        screen.blit(barrel_surf, barrel_rect)
-
-        #Player
-        player_grav += 0.3
-        player_rect.x += player_speed
-        player_rect.y += player_grav
-
-        if player_rect.bottom >= 496:
-            isGround = True
-            player_rect.bottom = 496
-
-        screen.blit(player_surf, player_rect)
+        
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+        
+        player.draw(screen)
+        player.update()
 
         #Collisions
-        if barrel_rect.colliderect(player_rect):
-                game_active = False
+        game_active = collision_sprite()
 
+    else:
+        screen.fill((16, 16, 16))
+        screen.blit(player_stand, player_stand_rect)
+        
+        screen.blit(name_surf, name_rect)
+        screen.blit(inst_surf, inst_rect)
 
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(60)
